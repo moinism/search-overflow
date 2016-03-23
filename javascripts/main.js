cdc5e4b99ca2738bf7e7f9702a406ee6055a085b
Moin Uddin <me@moin.im>
2016-03-23 22:27:37 +0500

    bit of explaination

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if(chrome.webstore && chrome.app && !chrome.app.isInstalled) {
       e.preventDefault();
       chrome.webstore.install('https://chrome.google.com/webstore/detail/mnjjnlpfohojhebjajehppdpepmhidnc', function () {
-        console.log('Installed, now go to Search Overflow tab');
+        console.log('Installed, now go to Search Overflow tab. If you had this open before installation then just close and open DevTools again.');
       });
     }
   });
