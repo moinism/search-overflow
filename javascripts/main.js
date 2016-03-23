03d9af6dab6f6d9aee8024f4a289aa519107dd5a
Moin Uddin <me@moin.im>
2016-03-23 22:19:02 +0500

    more changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('addToChrome').addEventListener('click', function (e) {
+    if(chrome.webstore && chrome.app && !chrome.app.isInstalled) {
+      e.preventDefault();
+      chrome.webstore.install('https://chrome.google.com/webstore/detail/mnjjnlpfohojhebjajehppdpepmhidnc', function () {
+        console.log('Installed, now go to Search Overflow tab');
+      });
+    }
+  });
+});
